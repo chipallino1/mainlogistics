3cdb6dd0ebc10913c9e6364bc2c30f79dd69f0ee
Yahor Skarupich <egsk@sam-solutions.net>
2018-10-22 13:47:21 +0300

    created table routes_info

--- db_sqlfiles/createtables.sql
+++ db_sqlfiles/createtables.sql
@@ -6,7 +6,7 @@ create table logistic_db.firms(
     id_address int not null,
     rating double,
     type varchar(45),
-    check('rating'>=0 | 'rating'<=5)
+    check(rating>=0 | rating<=5)
     
 );
 
@@ -29,8 +29,17 @@ create table logistic_db.orders(
 
 create table logistic_db.routes(
 	id int not null primary key auto_increment,
-    id_route int not null,
-    order_date date not null,
-    payment_day date not null
+    `from` varchar(45) not null,
+    `to` varchar(45) not null,
+    cost int not null,
+    id_route_info int not null
+);
+
+create table logistic_db.routes_info(
+	id int not null primary key auto_increment,
+    date_start date not null,
+    date_finish date not null,
+    optimality varchar(45),
+    length int not null
 );
 
