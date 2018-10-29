f9308e9c191f14c7b312e09fa87e0cbf8f8599dc
Yahor Skarupich <egsk@sam-solutions.net>
2018-10-29 18:01:30 +0300

    start of enter.js

---
+++ view/js/enter.js
@@ -0,0 +1,24 @@
+function loginPage() {
+	
+	let cont=document.getElementById('enter');
+	cont.appendChild();
+
+}
+
+function createDiv(className) {
+
+	let div=document.createElement('div');
+	div.className=className;
+	return div;
+
+}
+function createHeader(className) {
+	
+	let header=document.createElement('header');
+	header.className=className;
+	return header;
+
+}
+function createA(className,innerHTML,href) {
+	// body...
+}
