151013e45b238865e1ace81db027ec92200a2dcc
Yahor Skarupich <egsk@sam-solutions.net>
2018-11-01 13:15:22 +0300

    add new demo project(spring mvc)

---
+++ spring_test/demomvc/src/main/java/com/example/mvc/demomvc/DemomvcApplication.java
@@ -0,0 +1,12 @@
+package com.example.mvc.demomvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemomvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemomvcApplication.class, args);
+	}
+}
