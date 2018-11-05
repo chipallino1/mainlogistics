44ebc31f4e994c901d7324ba9a9960636ec186ee
Yahor Skarupich <egsk@sam-solutions.net>
2018-11-05 14:10:24 +0300

    authentication thymeleaf page

---
+++ logisctics/src/main/java/com/samsolutions/logistics/entities/User.java
@@ -0,0 +1,45 @@
+package com.samsolutions.logistics.entities;
+
+public class User {
+    private int Id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String phone;
+
+    public int getId() {
+        return Id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
---
+++ logisctics/src/test/java/com/samsolutions/logistics/entities/User.java
@@ -0,0 +1,3 @@
+package com.samsolutions.logistics.entities;
+
+
