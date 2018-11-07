d7eb6d5217212e86063f1f6b8762c54fbdd67d9e
chipallino1 <skorupich00@mail.ru>
2018-11-07 17:48:09 +0300

    validation added

--- mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/AuthenticationController.java
+++ mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/AuthenticationController.java
@@ -6,9 +6,12 @@ import com.samsolutions.logistics.mainlogistics.security.SaltHash;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.jws.WebParam;
 import javax.validation.Valid;
 import java.security.NoSuchAlgorithmException;
 
@@ -16,32 +19,37 @@ import java.security.NoSuchAlgorithmException;
 public class AuthenticationController {
 
     @RequestMapping(path = "auth",method = RequestMethod.GET)
-    public String authenticate( Model model){
+    public String authenticate(Model model){
 
         model.addAttribute("contactsEntity",new ContactsEntity());
         model.addAttribute("firmsEntity",new FirmsEntity());
 
-        try {
-            byte saltBytes[]=SaltHash.getSalt();
-           String secureHash= SaltHash.get_SHA_256_SecurePassword("10101998егор1",saltBytes);
-           if(SaltHash.validate("10101998егор",secureHash,saltBytes)){
-               System.out.println("true");
-           }else{
-               System.out.println("fasle");
-           }
-
-        } catch (NoSuchAlgorithmException e) {
-            e.printStackTrace();
-        }
-
         return "authentication";
 
     }
-    @RequestMapping(path = "contact",method = RequestMethod.POST)
-    public String contact(@Valid ContactsEntity contactsEntity,BindingResult bindingResult) {
+
+    @RequestMapping(path = "firm",method = RequestMethod.POST)
+    public String firm(@Valid FirmsEntity firmsEntity,BindingResult bindingResult, Model model) {
 
         if(bindingResult.hasErrors()){
 
+            model.addAttribute("contactsEntity",new ContactsEntity());
+            return "authentication";
+        }
+
+        return "index";
+
+    }
+    @RequestMapping(path = "auth",method = RequestMethod.POST)
+    public String registered(@Valid ContactsEntity contactsEntity, BindingResult bindingResultContacts,
+                             @Valid FirmsEntity firmsEntity,BindingResult bindingResultFirms, Model model) {
+
+
+        if(bindingResultContacts.hasErrors()){
+            model.addAttribute("firmsEntity",new FirmsEntity());
+            return "authentication";
+        }else if(bindingResultFirms.hasErrors()){
+            model.addAttribute("contactsEntity",new ContactsEntity());
             return "authentication";
         }
 
--- mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/entities/FirmsEntity.java
+++ mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/entities/FirmsEntity.java
@@ -1,6 +1,10 @@
 package com.samsolutions.logistics.mainlogistics.entities;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 @Entity
@@ -25,6 +29,9 @@ public class FirmsEntity {
 
     @Basic
     @Column(name = "firm_name")
+    @NotNull
+    @Size(min = 2,max=30)
+    @Pattern(regexp = "^[a-zA-z]+$")
     public String getFirmName() {
         return firmName;
     }
@@ -35,6 +42,20 @@ public class FirmsEntity {
 
     @Basic
     @Column(name = "email",nullable = false)
+    @NotNull
+    @Email(regexp = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+" +
+            "(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"" +
+            "(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x2" +
+            "1\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x" +
+            "09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z" +
+            "0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?" +
+            ":[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|" +
+            "[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\\.)" +
+            "{3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|" +
+            "[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\\x01-" +
+            "\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53" +
+            "-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\" +
+            "x7f])+)\\])")
     public String getEmail() {
 
         return email;
@@ -66,6 +87,8 @@ public class FirmsEntity {
 
     @Basic
     @Column(name = "firm_type")
+    @NotNull
+
     public String getFirmType() {
         return firmType;
     }
