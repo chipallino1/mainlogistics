4ed837923bf0d404c5a0e15795b056536c97136e
chipallino1 <skorupich00@mail.ru>
2018-11-10 20:19:00 +0300

    add login and profile page

--- mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/ProfileController.java
+++ mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/ProfileController.java
@@ -1,6 +1,10 @@
 package com.samsolutions.logistics.mainlogistics.controllers;
 
+import com.samsolutions.logistics.mainlogistics.dto.ContactDTO;
+import com.samsolutions.logistics.mainlogistics.dto.FirmDTO;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -8,8 +12,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class ProfileController {
 
     @RequestMapping(path = "profile/{type}",method = RequestMethod.GET)
-    public String profilePage(){
-        return "index";
+    public String profilePage(Model model,@ModelAttribute("contactDTO")ContactDTO contactDTO, @ModelAttribute("firmDTO")FirmDTO firmDTO){
+        if(contactDTO!=null){
+            model.addAttribute("profileName",contactDTO.getFirstName()+" "+ contactDTO.getLastName());
+            return "profile";
+        }
+        else{
+            model.addAttribute("profileName",firmDTO.getFirmName());
+            return "profile";
+        }
     }
 
 }
