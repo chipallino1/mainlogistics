f6dcfb8e8fbfbbf000e3a23da406d54c30f12f17
Yahor Skarupich <egsk@sam-solutions.net>
2019-01-21 18:34:28 +0300

    file upload started

--- jsf/practise/demo/src/main/java/com/example/demo/RegistrationBean.java
+++ jsf/practise/demo/src/main/java/com/example/demo/RegistrationBean.java
@@ -2,16 +2,21 @@ package com.example.demo;
 
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.event.ActionEvent;
+import javax.servlet.http.Part;
 import java.io.Serializable;
 
 @Scope("session")
 @Component
 public class RegistrationBean implements Serializable {
     private String login;
+    private Part file;
     private String password;
 
 
@@ -23,6 +28,14 @@ public class RegistrationBean implements Serializable {
         this.login = login;
     }
 
+    public Part getFile() {
+        return file;
+    }
+
+    public void setFile(Part file) {
+        this.file = file;
+    }
+
     public String getPassword() {
         return password;
     }
@@ -31,6 +44,11 @@ public class RegistrationBean implements Serializable {
         this.password = password;
     }
 
+    public void getData(ActionEvent actionEvent){
+        RegistrationBean user = (RegistrationBean) FacesContext.getCurrentInstance().
+                getExternalContext().getSessionMap().get("registrationBean");
+        System.out.println(user.getLogin()+" "+user.getPassword());
+    }
 
 
 }
