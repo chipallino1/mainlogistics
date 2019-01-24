c5bbe8610a8bc3befe69ea421bf8fdcca26dbb64
egsk <egsk@sam-solutions.com>
2019-01-24 16:50:48 +0300

    logistics on jsf start

--- mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/MainlogisticsApplication.java
+++ mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/MainlogisticsApplication.java
@@ -4,6 +4,12 @@ import com.samsolutions.logistics.mainlogistics.properties.FileStorageProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.faces.webapp.FacesServlet;
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 @EnableConfigurationProperties({
@@ -13,5 +19,16 @@ public class MainLogisticsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainLogisticsApplication.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean<FacesServlet> servletRegistrationBean() {
+        ServletRegistrationBean<FacesServlet> servletRegistrationBean = new ServletRegistrationBean<>(
+                new FacesServlet(), "*.xhtml");
+        servletRegistrationBean.setLoadOnStartup(1);
+        Map<String,String> initParams=new HashMap<>();
+        initParams.put("javax.faces.WEBAPP_RESOURCES_DIRECTORY","../resources");
+        servletRegistrationBean.setInitParameters(initParams);
+        return servletRegistrationBean;
     }
 }
