62f552b15722f44493cff8f8eec11a25c00e13ce
egsk <egsk@sam-solutions.com>
2019-01-29 17:08:40 +0300

    custom validation error messages

--- mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/backing/AuthenticationBBean.java
+++ mainlogistics/src/main/java/com/samsolutions/logistics/mainlogistics/controllers/backing/AuthenticationBBean.java
@@ -4,21 +4,21 @@ import com.samsolutions.logistics.mainlogistics.services.jsf.ComponentsContainer
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.faces.bean.ManagedBean;
+import javax.annotation.ManagedBean;
 import javax.faces.bean.NoneScoped;
 import java.io.Serializable;
 import java.util.Map;
 
-@Component
+@ManagedBean
 public class AuthenticationBBean implements Serializable {
 
-    @Autowired
-    private ComponentsContainer componentsContainer;
+    private String firstName;
 
-    public void show(){
-        Map<String,Object> attributesHide = componentsContainer.getElementById("id").getAttributes();
-        Map<String,Object> attributesShow = componentsContainer.getElementById("login").getAttributes();
-        attributesHide.putIfAbsent("style", "display: none;");
-        attributesShow.remove("style");
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 }
